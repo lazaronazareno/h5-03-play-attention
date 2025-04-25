1e11b37d98f65cbc57f69d81907e499076f7ea9d
alelex10 <elvizvida@gmail.com>
2025-04-24 22:31:02 -0300

    restablecimiento de page.tsx

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,9 +2,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <form action="/api/" method="POST">
-      </div>
+      <h1 className="text-4xl font-bold">Bienvenidos a Play Attention Argentina</h1>
     </main>
   );
 }
