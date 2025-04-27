f7ca6a720ee35fc80042949027d919c172e20bcb
Matias Paredes <tutecapo1985@gmail.com>
2025-04-27 14:07:23 -0300

    feat: Add brand imgs and create nav footer

---
+++ client/src/constants/navLinks.ts
@@ -0,0 +1,40 @@
+export const logoLinks = [
+	{
+		href: '/',
+		submenu: [
+			{ title: 'Play Attention Argentina', href: '/argentina' },
+			{ title: 'Banda BrainWare', href: '/banda' },
+			{ title: 'Profesionales', href: '/profesionales' },
+			{ title: 'Area de intervención', href: '/area' }
+		]
+	}
+]
+
+export const navLinks = [
+	{
+		title: 'Profesionales',
+		href: '/profesionales',
+		submenu: [
+			{ title: 'Soluciones', href: '/profesionales/soluciones' },
+			{ title: 'Metodologias de trabajo', href: '/profesionales/metodologias' }
+		]
+	},
+	{
+		title: 'Empresas',
+		href: '/empresas',
+		submenu: [
+			{ title: 'Empresas tecnologicas', href: '/empresas/tecnologicas' },
+			{ title: 'Empresas Deportivas', href: '/empresas/deportivas' }
+		]
+	},
+	{
+		title: 'Individuos',
+		href: '/individuos',
+		submenu: [
+			{ title: 'Niños | Adolescentes', href: '/individuos/ninos' },
+			{ title: 'Adultos', href: '/individuos/adultos' },
+			{ title: 'Planes de pago', href: '/individuos/planes' }
+		]
+	}
+]
+
