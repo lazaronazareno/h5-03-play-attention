922482c3bbcc2a5ecba89eb02617af6f9f640bad
lazaronazareno <lazaronazareno@gmail.com>
2025-04-28 20:38:23 -0300

    fix(frontend): :lipstick: validation styles updated

--- client/src/components/ui/ItemValidation.tsx
+++ client/src/components/ui/ItemValidation.tsx
@@ -4,16 +4,17 @@ import { CheckIcon } from '../ui/icons';
 interface ItemValidationProps {
   name: string
   icon?: React.ReactNode
+  className?: string
 }
 
-const ItemValidation = ({ name, icon }: ItemValidationProps) => {
+const ItemValidation = ({ name, icon, className }: ItemValidationProps) => {
   return (
-    <div className='bg-white text-violet-main flex gap-2.5 py-2 px-4 rounded-md text-xl font-poppins'>
+    <div className={`bg-white text-violet-main border border-violet-main flex items-center gap-2.5 py-2 px-4 rounded-md text-xl font-poppins font-medium ${className}`}>
       {icon ? (
         <span className='text-2xl'>{icon}</span>
       ) : (
-        <span className='bg-violet-main rounded-full size-5'>
-          <CheckIcon size={20} color='white' />
+        <span className='bg-violet-main rounded-full size-5 flex justify-center items-center'>
+          <CheckIcon size={18} color='white' strokeWidth={3} />
         </span>
       )}
       <p className='font-medium'>{name}</p>
