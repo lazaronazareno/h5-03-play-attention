f8cc119dcd860381e48591e224976ba8c219483c
lazaronazareno <lazaronazareno@gmail.com>
2025-04-28 18:24:41 -0300

    fix(frontend): :adhesive_bandage: typography component with better classes and added to heroSection

--- client/src/components/landing/HeroSection.tsx
+++ client/src/components/landing/HeroSection.tsx
@@ -6,9 +6,9 @@ import Typography from '../ui/Typography';
 
 const HeroSection = () => {
   return (
-    <div className="h-full p-20 flex flex-col items-center justify-center lg:flex-row gap-16 mx-auto">
+    <div className="h-full p-8 lg:p-20 flex flex-col items-center justify-center lg:flex-row gap-16 mx-auto">
       <div className='flex flex-col gap-7 max-w-[560px]'>
-        <Typography variant='h1' size='5xl' color='violet' weight='medium' text='Tecnología que entrena tu atención, transforma tu mente y potencia tu vida' />
+        <Typography variant='h1' size='title' color='violet' weight='medium' text='Tecnología que transforma tu mente y potencia tu vida' />
         <Typography variant='p' size='xl' color='default' weight='normal' text='Play Attention es un sistema de neuroentrenamiento para mejorar el desarrollo de habilidades cognitivas que impactan directamente en el rendimiento académico, laboral, deportivo y personal.' />
         <Button
           text='Realiza tu TEST de TDAH '
--- client/src/components/ui/Typography.tsx
+++ client/src/components/ui/Typography.tsx
@@ -5,7 +5,7 @@ interface TypographyProps {
   variant: 'h1' | 'h2' | 'h3' | 'p' | 'span';
   className?: string;
   color: 'default' | 'violet' | 'white';
-  size: 'xs' | 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | '5xl' | '6xl';
+  size: 'sm' | 'base' | 'lg' | 'xl' | '2xl' | '3xl' | '4xl' | 'subtitle' | 'title';
   weight: 'normal' | 'bold' | 'bolder' | 'lighter' | 'thin' | 'extralight' | 'light' | 'medium' | 'semibold' | 'extrabold' | 'black';
 }
 
@@ -19,10 +19,36 @@ const createTagComponent = (Tag: keyof JSX.IntrinsicElements) => {
   return Component;
 };
 
+const sizeClasses = {
+  sm: 'text-xs',
+  base: 'text-base lg:text-2xl',
+  lg: 'text-lg',
+  xl: 'text-xl',
+  '2xl': 'text-2xl',
+  '3xl': 'text-3xl',
+  '4xl': 'text-4xl',
+  'subtitle': 'text-3xl lg:text-5xl',
+  'title': 'text-3.5xl lg:text-6xl',
+}
+
+const weightClasses = {
+  normal: 'font-normal',
+  bold: 'font-bold',
+  bolder: 'font-bolder',
+  lighter: 'font-lighter',
+  thin: 'font-thin',
+  extralight: 'font-extralight',
+  light: 'font-light',
+  medium: 'font-medium',
+  semibold: 'font-semibold',
+  extrabold: 'font-extrabold',
+  black: 'font-black',
+}
+
 const Typography = ({ text, variant, className, color, size, weight }: TypographyProps) => {
   const colorClass = color === 'white' ? 'text-white' : color === 'violet' ? `text-violet-main` : '';
-  const sizeClass = 'text-' + size;
-  const weightClass = 'font-' + weight;
+  const sizeClass = sizeClasses[size] || 'text-base';
+  const weightClass = weightClasses[weight] || 'font-normal';
   const fontClass = variant === 'p' || variant === 'span' ? 'font-poppins' : 'font-roboto';
   return (
     <>
