a2ec4ebf1b8bcb47c4fc245c99940e6a32dfd518
Matias Paredes <tutecapo1985@gmail.com>
2025-05-02 17:52:21 -0300

    feat: Add form validations

--- client/src/app/(home)/layout.tsx
+++ client/src/app/(home)/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from "next";
 import { Poppins, Roboto } from "next/font/google";
-import "./globals.css";
 import Navbar from "@/components/layout/Navbar";
 import Footer from "@/components/layout/Footer";
 import "../globals.css";
