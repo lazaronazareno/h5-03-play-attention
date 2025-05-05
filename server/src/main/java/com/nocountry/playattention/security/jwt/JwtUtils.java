6c04ef55f17f4183584d6d089ef1e3fa1c2a0d97
alelex10 <elvizvida@gmail.com>
2025-05-05 19:53:37 -0300

    build: configure mail dependency and update JwtUtils- Added spring-boot-starter-mail dependency for email functionality- Added missing lombok dependency for mapstruct binding- Updated JwtUtils class to import SecretKey for JWT processing

--- server/src/main/java/com/nocountry/playattention/security/jwt/JwtUtils.java
+++ server/src/main/java/com/nocountry/playattention/security/jwt/JwtUtils.java
@@ -11,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
 import java.security.Key;
 import java.util.Date;
 
