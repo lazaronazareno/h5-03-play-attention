88d8ade894d94de4fa842a10889990d52145a424
lazaronazareno <lazaronazareno@gmail.com>
2025-05-05 11:51:29 -0300

    feat(frontend): :sparkles: config and support page with itemConfig and itemDefault components

--- client/src/app/(dashboard)/dashboard/config/page.tsx
+++ client/src/app/(dashboard)/dashboard/config/page.tsx
@@ -1,13 +1,81 @@
 
-import UserPanel from "@/components/dashboard/UserPanel";
+import ItemConfig from "@/components/dashboard/ItemConfig";
+import Button from "@/components/ui/Button";
+import Typography from "@/components/ui/Typography";
+import { Book, BookOpenIcon, Calendar, Star, Video, Youtube } from "lucide-react";
+import Image from "next/image";
 
-export default function EducationalMaterialPage() {
+export default function ConfigPage() {
+  const configOptions = [
+    {
+      title: "Nuevo Material Educativo",
+      icon: <BookOpenIcon size={28} color='white' strokeWidth={2} />,
+      name: "new_material",
+      status: true,
+    },
+    {
+      title: "Nuevo Tutorial",
+      icon: <Video size={28} color='white' strokeWidth={2} />,
+      name: "new_material",
+      status: true,
+    },
+    {
+      title: "Nuevo Video de Muestra",
+      icon: <Youtube size={28} color='white' strokeWidth={2} />,
+      name: "new_material",
+      status: false,
+    },
+    {
+      title: "Nuevos Artículos Medicos ",
+      icon: <Book size={28} color='white' strokeWidth={2} />,
+      name: "new_material",
+      status: false,
+    },
+    {
+      title: "Nuevo Material de Marketing",
+      icon: <Star size={28} color='white' strokeWidth={2} />,
+      name: "new_material",
+      status: true,
+    },
+    {
+      title: "Nuevos Eventos",
+      icon: <Calendar size={28} color='white' strokeWidth={2} />,
+      name: "new_material",
+      status: true,
+    },
+
+  ]
   return (
-    <UserPanel
-      title="Configuración"
-      description="Elegí las comunicaciones que querés recibir por este canal."
-      imageUrl="/dashboard/config.png"
-      items={[]}
-    />
+    <div className='flex flex-col px-6 pb-4 pt-8 gap-8 min-h-screen h-full w-full'>
+      <div className='flex justify-between'>
+        <div className='flex flex-col gap-4 justify-end'>
+          <Typography variant='h2' text={"Configuración"} color='violet' size='title' weight='medium' />
+          <Typography variant='p' text={"Elegí las comunicaciones que querés recibir por este canal."} color='default' size='base' weight='normal' />
+        </div>
+        <Image src={"/dashboard/config.png"} alt={"Configuración"} width={350} height={260} className='w-28 h-24 lg:w-[350px] lg:h-[260px]' />
+      </div>
+      <div className='relative flex flex-col gap-4 mt-4 border-t-2 border-t-violet-main bg-neutral-white2 h-full px-4 pt-9'>
+        <Button
+          text='Guardar Cambios'
+          variant='primary'
+          className="justify-center self-end"
+        />
+        {configOptions.length > 0 ? (
+          <>
+            {configOptions.map((item, index) => (
+              <ItemConfig
+                title={item.title}
+                icon={item.icon}
+                status={item.status}
+                name={item.name}
+                key={index}
+              />
+            ))}
+          </>
+        ) : (
+          <Typography variant='p' text='No se encontraron recursos actualmente' color='violet' size='base' weight='medium' />
+        )}
+      </div>
+    </div>
   );
 }
--- client/src/app/(dashboard)/dashboard/support/page.tsx
+++ client/src/app/(dashboard)/dashboard/support/page.tsx
@@ -1,12 +1,51 @@
-import UserPanel from "@/components/dashboard/UserPanel";
+import ItemDefault from "@/components/dashboard/ItemDefault";
+import { LeadForm } from "@/components/form-leads/LeadsForm";
+import Typography from "@/components/ui/Typography";
+import { FileText, MessageCircle, MessageSquare } from "lucide-react";
+import Image from "next/image";
 
-export default function EducationalMaterialPage() {
+const OPTIONS = [
+  {
+    title: "Formulario de contacto",
+    icon: <FileText size={28} color='white' strokeWidth={2} />,
+    children: <LeadForm />
+  },
+  {
+    title: "Chat en vivo",
+    icon: <MessageCircle size={28} color='white' strokeWidth={2} />,
+  },
+  {
+    title: "Foro",
+    icon: <MessageSquare size={28} color='white' strokeWidth={2} />,
+  },
+]
+
+export default function SupportPage() {
   return (
-    <UserPanel
-      title="Soporte al Cliente"
-      description="¿Cómo podemos ayudarte?"
-      imageUrl="/dashboard/soporte.png"
-      items={[]}
-    />
+    <div className='flex flex-col px-6 pt-8 gap-8 min-h-screen w-full'>
+      <div className='flex justify-between'>
+        <div className='flex flex-col gap-4 justify-end'>
+          <Typography variant='h2' text={"Soporte al Cliente"} color='violet' size='title' weight='medium' />
+          <Typography variant='p' text={"¿Cómo podemos ayudarte?"} color='default' size='base' weight='normal' />
+        </div>
+        <Image src={"/dashboard/soporte.png"} alt={"Soporte al Cliente"} width={350} height={260} className='w-28 h-24 lg:w-[350px] lg:h-[260px]' />
+      </div>
+      <div className='relative flex flex-col gap-4 mt-4 border-t-2 border-t-violet-main bg-neutral-white2 h-full px-4 pt-9'>
+        {OPTIONS.length > 0 ? (
+          <>
+            {OPTIONS.map((item, index) => (
+              <ItemDefault
+                title={item.title}
+                icon={item.icon}
+                component={item.children}
+                key={index}
+              />
+            ))}
+          </>
+        ) : (
+          <Typography variant='p' text='No se encontraron recursos actualmente' color='violet' size='base' weight='medium' />
+        )}
+      </div>
+    </div>
   );
 }
