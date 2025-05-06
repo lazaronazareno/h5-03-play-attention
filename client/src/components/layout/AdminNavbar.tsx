8f8492a33cc27119c148f1004917e4fa667d0f59
lazaronazareno <lazaronazareno@gmail.com>
2025-05-06 13:27:46 -0300

    fix: :sparkles: usePathname in adminNavbar

--- client/src/components/layout/AdminNavbar.tsx
+++ client/src/components/layout/AdminNavbar.tsx
@@ -5,15 +5,25 @@ import Button from '../ui/Button';
 import { Bell, LayoutGrid, Settings } from 'lucide-react';
 import { ADMIN_LINKS } from '@/constants/adminLinks';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const AdminNavbar = () => {
+  const pathname = usePathname();
+
   return (
     <header className='z-50 bg-neutral-white2 shadow-main flex items-center justify-between py-3 px-9 text-sm'>
       <div className='flex'>
         <Image src={"/branding/LogoWordAr.png"} alt='Avatar' width={32} height={24} className='me-6' />
         <div className='flex gap-5 items-center'>
           {ADMIN_LINKS.map((link) => (
-            <Link key={link.title} href={link.href} className='font-semibold hover:text-violet-main hover:font-bold'>{link.title}</Link>
+            <Link
+              key={link.title}
+              href={link.href}
+              className={`font-semibold hover:text-violet-main hover:font-bold ${pathname === link.href ? 'text-violet-main' : ''
+                }`}
+            >
+              {link.title}
+            </Link>
           ))}
         </div>
       </div>
